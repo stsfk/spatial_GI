451092f6096ab039363f0784836add293ee49538
s3886592 <yyang90@connect.hku.hk>
2019-06-02 01:36:16 +0800

    updates

--- ch2.R
+++ ch2.R
@@ -1,4 +1,7 @@
 library(sf)          # classes and functions for vector data
 library(raster)      # classes and functions for raster data
 
+library(spData)        # load geographic data
+library(spDataLarge)   # load larger geographic data
 
+head(world)
