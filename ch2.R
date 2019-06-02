2bb6fbcfcb64f59b73f1eff6946456237c0ba5fd
stsfk <yyang90@connect.hku.hk>
2019-06-02 18:39:00 +0800

    update

--- ch2.R
+++ ch2.R
@@ -5,3 +5,34 @@ library(spData)        # load geographic data
 library(spDataLarge)   # load larger geographic data
 
 head(world)
+
+st_point(c(5, 2))                 # XY point
+#> POINT (5 2)
+st_point(c(5, 2, 3))              # XYZ point
+#> POINT Z (5 2 3)
+st_point(c(5, 2, 1), dim = "XYM") # XYM point
+#> POINT M (5 2 1)
+st_point(c(5, 2, 3, 1))           # XYZM point
+#> POINT ZM (5 2 3 1)
+
+# the rbind function simplifies the creation of matrices
+## MULTIPOINT
+multipoint_matrix = rbind(c(5, 2), c(1, 3), c(3, 4), c(3, 2))
+st_multipoint(multipoint_matrix)
+#> MULTIPOINT (5 2, 1 3, 3 4, 3 2)
+## LINESTRING
+linestring_matrix = rbind(c(1, 5), c(4, 4), c(4, 1), c(2, 2), c(3, 2))
+st_linestring(linestring_matrix)
+#> LINESTRING (1 5, 4 4, 4 1, 2 2, 3 2)
+
+## POLYGON
+polygon_list = list(rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5)))
+st_polygon(polygon_list)
+
+## POLYGON with a hole
+polygon_border = rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5))
+polygon_hole = rbind(c(2, 4), c(3, 4), c(3, 3), c(2, 3), c(2, 4))
+polygon_with_hole_list = list(polygon_border, polygon_hole)
+st_polygon(polygon_with_hole_list)
+
+
