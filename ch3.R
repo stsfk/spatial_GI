18297e0c51a9e4eb4486808961e0ad75d8c859fe
stsfk <yyang90@connect.hku.hk>
2019-06-10 22:08:52 +0800

    update

--- ch3.R
+++ ch3.R
@@ -6,3 +6,10 @@ library(stringr) # for working with strings (pattern matching)
 library(spData)
 
 methods(class = "sf") # methods for sf objects, first 12 shown
+
+dim(world) # it is a 2 dimensional object, with rows and columns
+#> [1] 177  11
+nrow(world) # how many rows?
+#> [1] 177
+ncol(world) # how many columns?
+#> [1] 11
