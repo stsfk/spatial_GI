b151df97e197124cf87da17024c77b9dc5e63a17
stsfk <yyang90@connect.hku.hk>
2019-06-12 11:29:46 +0800

    update shape attribute operation

--- ch3.R
+++ ch3.R
@@ -13,3 +13,31 @@ nrow(world) # how many rows?
 #> [1] 177
 ncol(world) # how many columns?
 #> [1] 11
+
+sel_area = world$area_km2 < 10000
+summary(sel_area) # a logical vector
+#>    Mode   FALSE    TRUE 
+#> logical     170       7
+small_countries = world[sel_area, ]
+
+world_agg1 = aggregate(pop ~ continent, FUN = sum, data = world, na.rm = TRUE)
+class(world_agg1)
+#> [1] "data.frame"
+
+world_agg2 = aggregate(world["pop"], by = list(world$continent),
+                       FUN = sum, na.rm = TRUE)
+class(world_agg2)
+
+world %>%
+  group_by(continent) %>%
+  summarise(pop = sum(pop, na.rm = T)) %>%
+  arrange(desc(pop)) %>%
+  slice(1:3) %>%
+  st_drop_geometry()
+
+
+
+
+
+
+
