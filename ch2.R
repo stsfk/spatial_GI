144dc2fb7ed9cf063979b5959d5a715f4ccbfe3e
stsfk <yyang90@connect.hku.hk>
2020-06-01 15:03:59 +0800

    update plot functions

--- ch2.R
+++ ch2.R
@@ -4,7 +4,41 @@ library(raster)      # classes and functions for raster data
 library(spData)        # load geographic data
 library(spDataLarge)   # load larger geographic data
 
+library(tidyverse)
+
+# vignette("sf1")
+
+# Plot
 head(world)
+
+data_plot <- world %>%
+  mutate(pop_dens = pop/area_km2) 
+
+plot(data_plot["pop_dens"])
+
+world_asia = world[world$continent == "Asia", ]
+asia = st_union(world_asia)
+plot(world["pop"], reset = FALSE)
+plot(asia, add = TRUE, col = "red")
+
+
+plot(world["continent"], reset = FALSE)
+cex = sqrt(world$pop) / 10000
+world_cents = st_centroid(world, of_largest = TRUE)
+plot(st_geometry(world_cents), add = TRUE, cex = cex)
+
+
+india = world[world$name_long == "India", ]
+plot(st_geometry(india), expandBB = c(0, 0.2, 0.1, 1), col = "gray", lwd = 3)
+plot(world_asia[0], add = TRUE)
+
+
+
+
+
+
+
+
 
 st_point(c(5, 2))                 # XY point
 #> POINT (5 2)
