95c325bf8c2b3ab526842b1d61c501be68d080f2
stsfk <yyang90@connect.hku.hk>
2020-06-03 23:00:50 +0800

    update crs

--- ch2.R
+++ ch2.R
@@ -34,11 +34,7 @@ plot(world_asia[0], add = TRUE)
 
 
 
-
-
-
-
-
+# sfg data type, simple feature geometry
 
 st_point(c(5, 2))                 # XY point
 #> POINT (5 2)
@@ -58,6 +54,42 @@ st_multipoint(multipoint_matrix)
 linestring_matrix = rbind(c(1, 5), c(4, 4), c(4, 1), c(2, 2), c(3, 2))
 st_linestring(linestring_matrix)
 #> LINESTRING (1 5, 4 4, 4 1, 2 2, 3 2)
+
+
+# sfc simple feature columns
+point1 = st_point(c(5, 2))
+point2 = st_point(c(1, 3))
+points_sfc = st_sfc(point1, point2)
+points_sfc
+
+
+
+# sfc POLYGON
+polygon_list1 = list(rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5)))
+polygon1 = st_polygon(polygon_list1)
+polygon_list2 = list(rbind(c(0, 2), c(1, 2), c(1, 3), c(0, 3), c(0, 2)))
+polygon2 = st_polygon(polygon_list2)
+
+polygon_sfc = st_sfc(polygon1, polygon2)
+st_geometry_type(polygon_sfc)
+
+
+
+multilinestring_list1 = list(rbind(c(1, 5), c(4, 4), c(4, 1), c(2, 2), c(3, 2)), 
+                             rbind(c(1, 2), c(2, 4)))
+multilinestring1 = st_multilinestring((multilinestring_list1))
+multilinestring_list2 = list(rbind(c(2, 9), c(7, 9), c(5, 6), c(4, 7), c(2, 7)), 
+                             rbind(c(1, 7), c(3, 8)))
+multilinestring2 = st_multilinestring((multilinestring_list2))
+multilinestring_sfc = st_sfc(multilinestring1, multilinestring2)
+st_geometry_type(multilinestring_sfc)
+
+
+point_multilinestring_sfc = st_sfc(point1, multilinestring1)
+st_geometry_type(point_multilinestring_sfc)
+
+
+
 
 ## POLYGON
 polygon_list = list(rbind(c(1, 5), c(2, 2), c(4, 1), c(4, 4), c(1, 5)))
@@ -84,6 +116,52 @@ vector_filepath = system.file("vector/zion.gpkg", package = "spDataLarge")
 new_vector = st_read(vector_filepath)
 
 
+
+# sf class
+lnd_geom <- st_point(c(0.1, 51.5)) %>%
+  st_sfc(crs = 4326)
+lnd_attrib <- data.frame(
+  name = "London",
+  temperture = 25,
+  date = as.Date("2017-06-21")
+)
+lnd_sf <- st_sf(lnd_attrib, geomtry = lnd_geom)
+
+class(lnd_sf)
+
+
+# Raster ------------------------------------------------------------------
+library(sf)          # classes and functions for vector data
+library(raster)      # classes and functions for raster data
+
+library(spData)        # load geographic data
+library(spDataLarge)   # load larger geographic data
+
+library(tidyverse)
+
+raster_filepath = system.file("raster/srtm.tif", package = "spDataLarge")
+new_raster = raster(raster_filepath)
+
+plot(new_raster)
+
+
+# Geographic coordinate systems
+# The ellipsoid presentation and the precise relationship between 
+# cartesian coordinate and location on the Earth's surface
+
+# Projected coordinate reference systems
+# Project the geographic coordinate to 2d surface
+
+
+vector_filepath = system.file("vector/zion.gpkg", package = "spDataLarge")
+new_vector = st_read(vector_filepath)
+
+st_crs(new_vector) 
+
+new_vector = st_set_crs(new_vector, 4326) # set CRS
+st_crs(new_vector)
+
+projection(new_raster)
 
 
 
